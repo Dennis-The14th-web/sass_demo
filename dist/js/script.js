8d698c1edb0e8e2cd2221c1fc4848d16fa5f5a55
louis mane <dennis.itua1@gmail.com>
2021-09-16 05:41:36 -0400

    Updated all html files

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+const menuButton = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__hamburger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+
+const toggleMenu = () => {
+    if(!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(navItem => navItem.classList.add('open'));
+
+        showMenu = true;
+    } else {
+        hamburger.classList.remove('open')
+        nav.classList.remove('open')
+        menuNav.classList.remove('open')
+        navItems.forEach(navItem => navItem.classList.remove('open'));
+
+        showMenu = false;
+    }
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+
+
